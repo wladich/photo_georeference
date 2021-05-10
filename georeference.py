9e577eb31b247b833d61b15c9bf2dc56ef475d21
Sergey Orlov <wladimirych@gmail.com>
2021-05-10 22:24:33 +0300

    heavily refactor
    - switch to python3
    - use exiftool
    - remove precision considerations

--- georeference.py
+++ georeference.py
@@ -1,143 +1,114 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding: utf-8
 import argparse
 
 import datetime
 import calendar
 import time
-from PIL import Image
 import json
 import os
 import pyproj
+import subprocess
 
-from lib import PhotoSettingsStorage
-from lib.tracks import parse_gpx
-
-geod = pyproj.Geod(ellps='WGS84')
-
-
-def load_tracks(filenames):
-    all_segments = []
-    for fn in filenames:
-        segments, errors = parse_gpx(open(fn))
-        for er in errors:
-            print '%s: %s' % (fn, er)
-        segments = [seg for seg in segments if len(seg) > 1]
-        all_segments.extend(segments)
-    all_segments.sort(key=lambda seg: seg[0][2])
-    return all_segments
-
-
-def add_virtual_segments(segments):
-    for i in xrange(len(segments) - 1):
-        seg = segments[i]
-        next_seg = segments[i + 1]
-        segments.append([seg[-1], next_seg[0]])
+from gpx import parse_gpx
 
 
 def get_photo_local_timestamp(filename):
-    tags = Image.open(filename)._getexif()
-    datetime_original = tags[36867]
-    dt = datetime.datetime.strptime(datetime_original, '%Y:%m:%d %H:%M:%S')
+    res = subprocess.check_output(['exiftool', '-j', '-DateTimeOriginal', filename])
+    datetime_str = json.loads(res)[0]['DateTimeOriginal']
+    dt = datetime.datetime.strptime(datetime_str, '%Y:%m:%d %H:%M:%S')
     timestamp = calendar.timegm(dt.timetuple())
     return timestamp
 
 
-def interpolate_position(timestamp, seg_start, seg_end):
-    if seg_end[2] == seg_start[2]:
+def interpolate_latlon(timestamp, p1, p2):
+    if p1[2] == p2[2]:
         q = 0
     else:
-        q = float(timestamp - seg_start[2]) / (seg_end[2] - seg_start[2])
-    lat = seg_start[0] + (seg_end[0] - seg_start[0]) * q
-    lon = seg_start[1] + (seg_end[1] - seg_start[1]) * q
+        q = float(timestamp - p1[2]) / (p2[2] - p1[2])
+    lat = p1[0] + (p2[0] - p1[0]) * q
+    lon = p1[1] + (p2[1] - p1[1]) * q
     return lat, lon
 
 
-def get_photo_reference(filename, linestrings, time_offset, force_precise):
-    precise_reference_max_distance = 50
-    precise_reference_max_time_delta = 60
-    approx_reference_max_distance = 30000
+class GeoReferencer:
+    def __init__(self, track_files):
+        self.segments = []
+        self.load_tracks(track_files)
+        self.add_virtual_segments()
+        self.geod = pyproj.Geod(ellps='WGS84')
 
-    timestamp = get_photo_local_timestamp(filename)
-    timestamp += time_offset
-    ref = {
-        'timestamp': timestamp,
-        'coords_precision': None,
-        'track_heading': None,
-        'lat': None,
-        'lon': None
-    }
-    for linestring in linestrings:
-        if linestring[0][2] <= timestamp <= linestring[-1][2]:
-            prev_point = linestring[0]
-            for p in linestring[1:]:
-                if prev_point[2] <= timestamp <= p[2]:
-                    az,_, dist = geod.inv(prev_point[1], prev_point[0], p[1], p[0])
-                    if az < 0:
-                        az += 360
-                    time_delta = p[2] - prev_point[2]
-                    if dist <= approx_reference_max_distance:
-                        lat, lon = interpolate_position(timestamp, prev_point, p)
-                        ref['lat'] = lat
-                        ref['lon'] = lon
-                        ref['trkpt_dist'] = dist
-                        ref['trkpt_time_delta'] = time_delta
-                        if ((time_delta <= precise_reference_max_time_delta and dist <= precise_reference_max_distance)
-                            or force_precise):
-                            ref['coords_precision'] = 'precise'
-                            ref['track_heading'] = az
-                        else:
-                            ref['coords_precision'] = 'approx'
-                    return ref
-                prev_point = p
-    return ref
+    def load_tracks(self, filenames):
+        for fn in filenames:
+            with open(fn) as f:
+                segments = parse_gpx(f)
+            self.segments += [seg for seg in segments if len(seg) > 1]
+        self.segments.sort(key=lambda seg: seg[0][2])
+
+    def add_virtual_segments(self):
+        for i in range(len(self.segments) - 1):
+            seg = self.segments[i]
+            next_seg = self.segments[i + 1]
+            p1 = seg[-1]
+            p2 = next_seg[0]
+            if p2[2] > p1[2]:
+                self.segments.append([seg[-1], next_seg[0]])
+
+    def get_position_from_timestamp(self, timestamp, time_offset):
+        timestamp += time_offset
+        position = {
+            'timestamp': timestamp,
+        }
+        segment_for_point = None
+        for segment in self.segments:
+            if segment[0][2] <= timestamp <= segment[-1][2]:
+                segment_for_point = segment
+                break
+        if segment_for_point:
+            for i in range(len(segment_for_point) - 1):
+                p1 = segment_for_point[i]
+                p2 = segment_for_point[i + 1]
+                if p1[2] <= timestamp <= p2[2]:
+                    az, _, track_points_dist = self.geod.inv(p1[1], p1[0], p2[1], p2[0])
+                    track_points_time_delta = p2[2] - p1[2]
+                    position['track_points_dist'] = track_points_dist
+                    position['track_points_time_delta'] = track_points_time_delta
+                    lat, lon = interpolate_latlon(timestamp, p1, p2)
+                    position['lat'] = lat
+                    position['lon'] = lon
+                    position['heading'] = az
+                    break
+        return position
 
 
-def store_tracks(segments, dir_):
-    tracks_filename = os.path.join(dir_, '_tracks.json')
-    with open(tracks_filename, 'w') as f:
-        json.dump(segments, f)
+def get_default_timezone():
+    return -time.timezone / 3600
+
+
+def calculate_offset(camera_time_zone_hours, camera_minus_gps_seconds):
+    return -camera_time_zone_hours * 3600 + camera_minus_gps_seconds
+
+
+def georefence_images_from_exif(images, tracks, time_offset):
+    referencer = GeoReferencer(tracks)
+    positions = {}
+    for filename in images:
+        timestamp = get_photo_local_timestamp(filename)
+        positions[filename] = referencer.get_position_from_timestamp(timestamp, time_offset)
+    return positions
 
 
 def main():
-    default_timezone = -time.timezone / 3600
+    default_timezone = get_default_timezone()
     parser = argparse.ArgumentParser()
-    parser.add_argument('src_dir')
-    parser.add_argument('track', nargs='+')
+    parser.add_argument('--images', nargs='+', required=True)
+    parser.add_argument('--tracks', nargs='+', required=True)
     parser.add_argument('-z', '--timezone', default=default_timezone, type=float, help='default=%s' % default_timezone)
-    parser.add_argument('-o', '--offset', default=0, type=int, help='GPS time - camera time')
-    parser.add_argument('-f', '--force-precise', action='store_true')
+    parser.add_argument('-o', '--offset', default=0, type=int, help='GPS time - camera time, in seconds')
     conf = parser.parse_args()
 
-    dir_ = conf.src_dir
-
-    segments = load_tracks(conf.track)
-    store_tracks(segments, dir_)
-    add_virtual_segments(segments)
-    # FIXME: проверить знак offset, сделать как в других утилитах
-    offset = -conf.timezone * 3600 + conf.offset
-
-    photo_setting = PhotoSettingsStorage(dir_)
-    for filename in sorted(os.listdir(dir_)):
-        if os.path.splitext(filename)[1].lower() == '.jpg':
-            ref = get_photo_reference(os.path.join(dir_, filename), segments, offset, conf.force_precise)
-            stored_record = photo_setting.get(filename)
-            if stored_record and stored_record.get('coords_precision') == 'manual':
-                print 'Photo %s has coordinates set manually, refusing to update' % filename
-                continue
-            precision = ref['coords_precision']
-            time_delta = ref.pop('trkpt_time_delta', None)
-            trkpt_dist = ref.pop('trkpt_dist', None)
-            if not precision:
-                print 'Photo %s: could not get reference from track' % filename
-            elif precision == 'precise':
-                pass
-            elif precision == 'approx':
-                print 'Photo %s: approximate coordinates written, dt=%s, dl=%s' % (filename, time_delta, trkpt_dist)
-            else:
-                raise Exception(ref)
-            photo_setting.set(filename, ref)
-
+    for k, v in georefence_images_from_exif(conf.images, conf.tracks, calculate_offset(conf.timezone, conf.offset)).items():
+        print(k, v)
 
 if __name__ == '__main__':
     main()
---
+++ gpx.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+from xml.dom import minidom
+from xml.parsers.expat import ExpatError
+import calendar
+import re
+
+
+def check_segment_time_order(points):
+    if not points:
+        return
+    for i in range(len(points) - 1):
+        delta = points[i + 1][2] < points[i][2]
+        if delta < 0:
+            raise Exception('points time out of order (point #%s, %s seconds)' % (i + 2, delta))
+
+
+def parse_time(s):
+    m = re.match(r'^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)Z$', s)
+    groups = m.groups()
+    year, month, day, hour, minutes = map(int, groups[:-1])
+    seconds = float(groups[-1])
+    return calendar.timegm((year, month, day, hour, minutes, seconds))
+
+
+def parse_gpx(fp):
+    segments = []
+    try:
+        gpx = minidom.parse(fp)
+    except ExpatError as e:
+        raise Exception('Invalid gpx file: %s' % e)
+    else:
+        for trk_i, trk in enumerate(gpx.getElementsByTagName('trk'), 1):
+            for seg_i, trkseg in enumerate(trk.getElementsByTagName('trkseg'), 1):
+                linestring = []
+                for pt_i, trkpt in enumerate(trkseg.getElementsByTagName('trkpt'), 1):
+                    lat = float(trkpt.getAttribute('lat'))
+                    lon = float(trkpt.getAttribute('lon'))
+                    time_ = trkpt.getElementsByTagName('time')
+                    if not time_:
+                        raise Exception('No time in track point (track #%s, segment #%s, point #%s), skipping segment'
+                                        % (trk_i, seg_i, pt_i))
+                    time_ = time_[0].childNodes[0].data
+                    timestamp = parse_time(time_)
+                    linestring.append((lat, lon, timestamp))
+                if linestring:
+                    check_segment_time_order(linestring)
+                    segments.append(linestring)
+    return segments
