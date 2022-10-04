9077bbf6812b62267f00edee3f12ecb107c24d6e
Sergej Orlov <wladimirych@gmail.com>
2022-10-04 20:33:05 +0200

    Fix linting errors

--- photo_georeference/georeference.py
+++ photo_georeference/georeference.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import argparse
-import datetime
 import calendar
+import datetime
 import json
 import subprocess
 from dataclasses import dataclass
@@ -18,30 +18,30 @@ class Segment:
 
 
 def get_photo_local_timestamp(filename):
-    res = subprocess.check_output(['exiftool', '-j', '-DateTimeOriginal', filename])
-    datetime_str = json.loads(res)[0]['DateTimeOriginal']
-    dt = datetime.datetime.strptime(datetime_str, '%Y:%m:%d %H:%M:%S')
-    timestamp = calendar.timegm(dt.timetuple())
+    res = subprocess.check_output(["exiftool", "-j", "-DateTimeOriginal", filename])
+    datetime_str = json.loads(res)[0]["DateTimeOriginal"]
+    datetime_ = datetime.datetime.strptime(datetime_str, "%Y:%m:%d %H:%M:%S")
+    timestamp = calendar.timegm(datetime_.timetuple())
     return timestamp
 
 
-def interpolate_latlon(timestamp, p1, p2):
-    if p1[2] == p2[2]:
+def interpolate_latlon(timestamp, point1, point2):
+    if point1[2] == point2[2]:
         q = 0
     else:
-        q = float(timestamp - p1[2]) / (p2[2] - p1[2])
-    lat = p1[0] + (p2[0] - p1[0]) * q
-    lon = p1[1] + (p2[1] - p1[1]) * q
+        q = float(timestamp - point1[2]) / (point2[2] - point1[2])
+    lat = point1[0] + (point2[0] - point1[0]) * q
+    lon = point1[1] + (point2[1] - point1[1]) * q
     return lat, lon
 
 
 def load_tracks_flatten_segments(filenames):
     segments = []
-    for fn in filenames:
-        with open(fn) as f:
+    for filename in filenames:
+        with open(filename, "rb") as f:
             for track_seg in parse_gpx(f):
                 if len(track_seg) > 1:
-                    segments.append(Segment([fn], track_seg))
+                    segments.append(Segment([filename], track_seg))
     segments.sort(key=lambda seg: seg.points[0][2])
     return segments
 
@@ -53,26 +53,31 @@ class GeoReferencer:
     def __init__(self, track_files):
         self.segments = load_tracks_flatten_segments(track_files)
         self.add_virtual_segments()
-        self.geod = pyproj.Geod(ellps='WGS84')
+        self.geod = pyproj.Geod(ellps="WGS84")
 
     def add_virtual_segments(self):
         for i in range(len(self.segments) - 1):
             seg = self.segments[i]
             next_seg = self.segments[i + 1]
-            p1 = seg.points[-1]
-            p2 = next_seg.points[0]
-            if p2[2] > p1[2]:
-                self.segments.append(Segment(seg.track_names + next_seg.track_names, [p1, p2]))
+            point1 = seg.points[-1]
+            point2 = next_seg.points[0]
+            if point2[2] > point1[2]:
+                self.segments.append(
+                    Segment(seg.track_names + next_seg.track_names, [point1, point2])
+                )
 
-    def calculate_heading(self, lat: float, lon: float, timestamp: int, segment: Segment, ind: int):
+    # pylint: disable-next=too-many-arguments
+    def calculate_heading(
+        self, lat: float, lon: float, timestamp: int, segment: Segment, ind: int
+    ):
         segment_points = segment.points
 
         def is_point_in_range(ind):
-            p = segment_points[ind]
-            time_delta = abs(timestamp - p[2])
+            point = segment_points[ind]
+            time_delta = abs(timestamp - point[2])
             if time_delta > self.heading_smoothing_max_time_delta:
                 return False
-            _, _, dist = self.geod.inv(lon, lat, p[1], p[0])
+            _, _, dist = self.geod.inv(lon, lat, point[1], point[0])
             return dist < self.heading_smoothing_target_distance
 
         ind2 = ind + 1
@@ -81,15 +86,15 @@ class GeoReferencer:
             ind -= 1
         while ind2 < len(segment.points) - 1 and is_point_in_range(ind2):
             ind2 += 1
-        p1 = segment_points[ind]
-        p2 = segment_points[ind2]
-        heading, _, _ = self.geod.inv(p1[1], p1[0], p2[1], p2[0])
+        point1 = segment_points[ind]
+        point2 = segment_points[ind2]
+        heading, _, _ = self.geod.inv(point1[1], point1[0], point2[1], point2[0])
         return heading
 
     def get_position_from_timestamp(self, timestamp, time_offset):
         timestamp += time_offset
         position = {
-            'timestamp': timestamp,
+            "timestamp": timestamp,
         }
         segment_for_point = None
         for segment in self.segments:
@@ -98,19 +103,23 @@ class GeoReferencer:
                 break
         if segment_for_point:
             for i in range(len(segment_for_point.points) - 1):
-                p1 = segment_for_point.points[i]
-                p2 = segment_for_point.points[i + 1]
-                if p1[2] <= timestamp <= p2[2]:
-                    _, _, track_points_dist = self.geod.inv(p1[1], p1[0], p2[1], p2[0])
-                    track_points_time_delta = p2[2] - p1[2]
-                    position['track_points_dist'] = track_points_dist
-                    position['track_points_time_delta'] = track_points_time_delta
-                    lat, lon = interpolate_latlon(timestamp, p1, p2)
-                    heading = self.calculate_heading(lat, lon, timestamp, segment_for_point, i)
-                    position['lat'] = lat
-                    position['lon'] = lon
-                    position['heading'] = heading
-                    position['sources'] = segment_for_point.track_names
+                point1 = segment_for_point.points[i]
+                point2 = segment_for_point.points[i + 1]
+                if point1[2] <= timestamp <= point2[2]:
+                    _, _, track_points_dist = self.geod.inv(
+                        point1[1], point1[0], point2[1], point2[0]
+                    )
+                    track_points_time_delta = point2[2] - point1[2]
+                    position["track_points_dist"] = track_points_dist
+                    position["track_points_time_delta"] = track_points_time_delta
+                    lat, lon = interpolate_latlon(timestamp, point1, point2)
+                    heading = self.calculate_heading(
+                        lat, lon, timestamp, segment_for_point, i
+                    )
+                    position["lat"] = lat
+                    position["lon"] = lon
+                    position["heading"] = heading
+                    position["sources"] = segment_for_point.track_names
                     break
         return position
 
@@ -123,8 +132,10 @@ def georefence_images_from_exif(images, tracks, time_offset):
     """
     :param images: list of file names
     :param tracks: list of gpx files
-    :param time_offset: GPS time (UTC) - camera time (local), in seconds, combined from time zone and camera clock skew
-    :return: list of dicts with keys: lat, lon, heading, track_points_dist, track_points_time_delta
+    :param time_offset: GPS time (UTC) - camera time (local), in seconds,
+        combined from time zone and camera clock skew
+    :return: list of dicts with keys:
+        lat, lon, heading, track_points_dist, track_points_time_delta
         track_points_dist - distance between track points prior and after the photo
         track_points_time_delta - time between track points prior and after the photo
     """
@@ -132,21 +143,33 @@ def georefence_images_from_exif(images, tracks, time_offset):
     positions = {}
     for filename in images:
         timestamp = get_photo_local_timestamp(filename)
-        positions[filename] = referencer.get_position_from_timestamp(timestamp, time_offset)
+        positions[filename] = referencer.get_position_from_timestamp(
+            timestamp, time_offset
+        )
     return positions
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--images', nargs='+', required=True)
-    parser.add_argument('--tracks', nargs='+', required=True)
-    parser.add_argument('-z', '--timezone', required=True, type=float, help='Time offset from UTC in hours')
-    parser.add_argument('-o', '--offset', default=0, type=int, help='GPS time - camera time, in seconds')
+    parser.add_argument("--images", nargs="+", required=True)
+    parser.add_argument("--tracks", nargs="+", required=True)
+    parser.add_argument(
+        "-z",
+        "--timezone",
+        required=True,
+        type=float,
+        help="Time offset from UTC in hours",
+    )
+    parser.add_argument(
+        "-o", "--offset", default=0, type=int, help="GPS time - camera time, in seconds"
+    )
     conf = parser.parse_args()
 
-    refs = georefence_images_from_exif(conf.images, conf.tracks, calculate_offset(conf.timezone, conf.offset))
+    refs = georefence_images_from_exif(
+        conf.images, conf.tracks, calculate_offset(conf.timezone, conf.offset)
+    )
     print(json.dumps(refs, indent=2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- photo_georeference/gpx.py
+++ photo_georeference/gpx.py
@@ -1,8 +1,8 @@
 # coding: utf-8
-from xml.dom import minidom
-from xml.parsers.expat import ExpatError
 import calendar
 import re
+from xml.dom import minidom
+from xml.parsers.expat import ExpatError
 
 
 def check_segment_time_order(points):
@@ -11,12 +11,16 @@ def check_segment_time_order(points):
     for i in range(len(points) - 1):
         delta = points[i + 1][2] < points[i][2]
         if delta < 0:
-            raise Exception('points time out of order (point #%s, %s seconds)' % (i + 2, delta))
+            raise Exception(
+                "points time out of order (point #%s, %s seconds)" % (i + 2, delta)
+            )
 
 
-def parse_time(s):
-    m = re.match(r'^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)Z$', s)
-    groups = m.groups()
+def parse_time(time_str):
+    match = re.match(
+        r"^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d+)?)Z$", time_str
+    )
+    groups = match.groups()
     year, month, day, hour, minutes = map(int, groups[:-1])
     seconds = float(groups[-1])
     return calendar.timegm((year, month, day, hour, minutes, seconds))
@@ -27,18 +31,20 @@ def parse_gpx(fp):
     try:
         gpx = minidom.parse(fp)
     except ExpatError as e:
-        raise Exception('Invalid gpx file: %s' % e)
+        raise Exception("Invalid gpx file: %s" % e) from e
     else:
-        for trk_i, trk in enumerate(gpx.getElementsByTagName('trk'), 1):
-            for seg_i, trkseg in enumerate(trk.getElementsByTagName('trkseg'), 1):
+        for trk_i, trk in enumerate(gpx.getElementsByTagName("trk"), 1):
+            for seg_i, trkseg in enumerate(trk.getElementsByTagName("trkseg"), 1):
                 linestring = []
-                for pt_i, trkpt in enumerate(trkseg.getElementsByTagName('trkpt'), 1):
-                    lat = float(trkpt.getAttribute('lat'))
-                    lon = float(trkpt.getAttribute('lon'))
-                    time_ = trkpt.getElementsByTagName('time')
+                for pt_i, trkpt in enumerate(trkseg.getElementsByTagName("trkpt"), 1):
+                    lat = float(trkpt.getAttribute("lat"))
+                    lon = float(trkpt.getAttribute("lon"))
+                    time_ = trkpt.getElementsByTagName("time")
                     if not time_:
-                        raise Exception('No time in track point (track #%s, segment #%s, point #%s)'
-                                        % (trk_i, seg_i, pt_i))
+                        raise Exception(
+                            "No time in track point (track #%s, segment #%s, point #%s)"
+                            % (trk_i, seg_i, pt_i)
+                        )
                     time_ = time_[0].childNodes[0].data
                     timestamp = parse_time(time_)
                     linestring.append((lat, lon, timestamp))
